bc4806b7ac002415f8e111254951e927fda791a2
AdidayaOsha <adidayaosha@gmail.com>
2022-03-16 15:15:55 +0700

    added Register and Login feature

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+const Container = styled.div``;
+const Wrapper = styled.div``;
+const Cart = () => {
+  return (
+    <Container>
+      <Wrapper></Wrapper>
+    </Container>
+  );
+};
+
+export default Cart;
